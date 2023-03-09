d3be25359786c036446b1a85e29a71f31b6db203
James Ekunola <jamesekunola@gmail.com>
2023-03-09 11:22:36 +0100

    Fetch server result and update user search UI

--- app.js
+++ app.js
@@ -1,32 +1,83 @@
-// variables
+// map layer
+const googleStreets = L.tileLayer(
+  "http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
+);
+// map icon
+const myIcon = L.icon({
+  iconUrl: "/images/icon-location.svg",
+  iconSize: [40, 60],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+  shadowUrl: "",
+  shadowSize: [68, 95],
+  shadowAnchor: [22, 94],
+});
+
+let map;
+const inputEl = document.querySelector("input[type= 'text']");
+const ipAddressEl = document.querySelector(".ip");
+const locationEl = document.querySelector(".location");
+const timezoneEl = document.querySelector(".timezone");
+const ispEl = document.querySelector(".isp");
+const submitBtn = document.querySelector(".submit");
+const resultBox = document.querySelector(".result");
 
 // event listener
 window.addEventListener("DOMContentLoaded", () => {
-  mapInit();
+  renderUserSearchResult();
+  renderMap();
+});
+
+submitBtn.addEventListener("click", renderUserSearchResult);
+window.addEventListener("keyup", (e) => {
+  if (e.key == "Enter") {
+    renderUserSearchResult();
+  }
 });
 
 // function
 
-function mapInit() {
-  const map = L.map("map").setView([51.505, -0.09], 13);
-
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
-
-  L.marker([51.5, -0.09]).addTo(map);
-
-  const leafletMarker = document.querySelector(".leaflet-marker-pane img");
-
-  // change maker img
-  leafletMarker.src = "/images/icon-location.svg";
+function renderMap(x = 34.04915, y = -118.09462) {
+  map = L.map("map").setView([x, y], 13);
+  googleStreets.addTo(map);
+  L.marker([x, y], { icon: myIcon }).addTo(map);
 }
 
-const url = `https://geo.ipify.org/api/v2/country?apiKey=at_fugKaXsYHQa25CDw9tqJIVAeve5w3&ipAddress=8.8.8.8`;
+function renderUserSearchResult() {
+  const userInput = inputEl.value;
 
-fetch(url)
-  .then((res) => res.json())
-  .then((result) => {
-    console.log(result);
-  });
+  // fetch data if input box isn't empty
+  if (userInput) {
+    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_fugKaXsYHQa25CDw9tqJIVAeve5w3&ipAddress= ${userInput}`;
+
+    fetchData(url);
+
+    inputEl.value = "";
+  }
+}
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  // render search result on user interface
+  renderSearchResult(data);
+}
+
+function renderSearchResult(data) {
+  ipAddressEl.innerHTML = data.ip;
+  locationEl.innerHTML = data.location.city;
+  ispEl.innerHTML = data.isp;
+  timezoneEl.innerHTML = `UTC${data.location.timezone}`;
+
+  const lat = data.location.lat;
+  const lng = data.location.lng;
+
+  map = map.remove();
+  renderMap(lat, lng);
+}
